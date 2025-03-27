db67fa15424f0ea605d6fa214062a6d9358ce363
honkak <kindkyw@gmail.com>
2025-03-28 01:10:25 +0900

    Update index.js and add app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to My Webpage</h1>
+      <p>This is a simple webpage created with Cursor AI.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
--- index.js
+++ index.js
@@ -7,4 +7,5 @@ export default function Home() {
       </div>
     );
   }
+  print(3)
   
